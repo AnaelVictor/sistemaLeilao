a7ad46b43a08be024b69dca3a7f4b2aedcb861e3
AnaelVictor <anaelvictorba@gmail.com>
2024-01-09 00:09:49 -0300

    Link entre listagem de produtos e listagem de vendas adicionado ao botao de consultar vendas

--- src/VendasVIEW.java
+++ src/VendasVIEW.java
@@ -11,12 +11,12 @@ import javax.swing.table.DefaultTableModel;
  *
  * @author Usuario
  */
-public class VendasVIEW extends javax.swing.JFrame {
+public class vendasVIEW extends javax.swing.JFrame {
 
     /**
      * Creates new form VendasVIEW
      */
-    public VendasVIEW() {
+    public vendasVIEW() {
         initComponents();
         listarVendas();
     }
@@ -117,20 +117,21 @@ public class VendasVIEW extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(VendasVIEW.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(vendasVIEW.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(VendasVIEW.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(vendasVIEW.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(VendasVIEW.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(vendasVIEW.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(VendasVIEW.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(vendasVIEW.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new VendasVIEW().setVisible(true);
+                new vendasVIEW().setVisible(true);
             }
         });
     }
